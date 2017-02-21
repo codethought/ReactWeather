1f9ab4a7042749eb028a103fbfa923edabb7881b
David Orriss Jr <dorriss@expedia.com>
2017-02-21 09:02:34 -0800

    completed lecture 32

---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+var WeatherMessage = React.createClass({
+  render:function() {
+    return (
+      <h3>It's 42 in Bellevue</h3>
+    );
+  }
+});
+
+module.exports = WeatherMessage;
