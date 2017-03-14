6ca663bb859bf463a7892ef3b5d2092d329b65b5
David Orriss Jr <davido@codethought.com>
2017-03-13 18:16:31 -0700

    completed lecture 40

---
+++ example-arrow-functions.js
@@ -0,0 +1,36 @@
+var names = ['Dave','Kat','Isabelle'];
+
+names.forEach(function(name) {
+  console.log('forEach', name);
+});
+
+names.forEach((name) => {
+  console.log('arrowFunc', name);
+});
+
+names.forEach((name) => console.log(name));
+
+var returnMe = (name) => name + '!';
+console.log(returnMe('David'));
+
+var person = {
+  name: 'David',
+  greet: function () {
+      names.forEach ( (name) => {
+        console.log(this.name + ' says hi to ' + name);
+      });
+  }
+};
+
+person.greet();
+
+//addExpression
+var addExpression = (a, b) => (a + b);
+
+// addStatement
+var addStatement = (a, b) => {
+  return a + b;
+};
+
+console.log(addExpression(1,2));
+console.log(addStatement(3, 4));
