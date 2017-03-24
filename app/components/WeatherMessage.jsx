7c4084fa5252be7a5560e29bb16325200ffc98fb
David Orriss Jr <dorriss@expedia.com>
2017-03-24 15:15:04 -0700

    completed lecture 41

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,13 +1,9 @@
 const React = require('react');
 
-var WeatherMessage = React.createClass({
-  render:function() {
-    var {temp, location} = this.props;
-    
-    return (
-      <h3>It's {temp} in {location}</h3>
-    );
-  }
-});
+let WeatherMessage = ({temp, location}) => {
+  return (
+    <h3>It's {temp} in {location}</h3>
+  );
+};
 
 module.exports = WeatherMessage;
